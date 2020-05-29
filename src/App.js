b3dc9a7e17bcbda91334e0dcd33afe229402ba44
Meweezy <muizz.bolaji@yahoo.com>
2020-05-30 00:02:13 +0200

    additional firebase files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import db from "./firebase";
 import "./App.css";
 import Todo from "./Todo";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [scaleIn, setScaleIn] = useState(false);
+
+  useEffect(() => {
+    db.collection("todos").onSnapshot((snapshot) => {
+      // console.log(snapshot.docs.map((doc) => doc.data()));
+      setTodos(snapshot.docs.map((doc) => doc.data().title));
+      setScaleIn(true);
+    });
+  }, []);
+
+  const deleteTodo = (e) => {
+    let index = e.target.value;
+    db.collection("todos")
+      .where("title", "==", todos[index])
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          // console.log(doc);
+          db.collection("todos").doc(doc.id).delete();
+        });
+      });
+  };
 
   const addTodo = (e) => {
     e.preventDefault();
@@ -13,29 +37,58 @@ function App() {
     //add what is in iput to the todos array
     //clear the input field
 
-    setTodos([...todos, input]);
+    // setTodos([...todos, input]);
+
+    db.collection("todos").add({
+      title: input,
+    });
     setInput("");
+    // setScaleIn(true);
 
     console.log([...todos, input]);
   };
 
   return (
-    <div className="App">
+    <div id="bootstrap-overrides" className="App">
       <h1>Todo App</h1>
-      <form>
+      <form className="form-inline">
         <input
+          className="form-control todo-input"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           type="text"
         ></input>
-        <button disabled={!input} type="submit" onClick={addTodo}>
-          Add Todo
+        <button
+          className="addBtn"
+          disabled={!input}
+          type="submit"
+          onClick={addTodo}
+        >
+          ADD TODO
         </button>
       </form>
 
-      {todos.map((todo, i) => (
-        <Todo title={todo} key={i}></Todo>
-      ))}
+      <CSSTransition
+        in={scaleIn}
+        timeout={300}
+        classNames="scaleIn"
+        unmountOnExit
+        onEnter={() => setScaleIn(true)}
+        onEntered={() => setScaleIn(true)}
+        onExit={() => setScaleIn(true)}
+        onExited={() => setScaleIn(true)}
+      >
+        <div className="todo-wrapper">
+          {todos.map((todo, index) => (
+            <Todo
+              title={todo}
+              value={index}
+              key={index}
+              deleteTodo={deleteTodo}
+            ></Todo>
+          ))}
+        </div>
+      </CSSTransition>
     </div>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Todo.css";
+// import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Todo = (props) => {
   return (
-    <div>
-      <h1>{props.title}</h1>
+    <div className="todo">
+      <div className="todo-span">
+        <span data-id={props.id}>{props.title}</span>
+      </div>
+
+      <button
+        value={props.value}
+        onClick={props.deleteTodo}
+        className="deleteBtn"
+      >
+        {/* <FontAwesomeIcon className="deleteIcon" icon={faTrashAlt} /> */}
+      </button>
     </div>
   );
 };
